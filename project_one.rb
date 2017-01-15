18a1602e8432efc882e2ae21293a57df5535a3ba
Alan Cruse <alancruse86@gmail.com>
2017-01-15 17:50:01 +0100

    begun bubble sort by block argument

--- project_one.rb
+++ project_one.rb
@@ -6,6 +6,7 @@ bubble_sort([4,3,78,2,0,2])
 
 def bubble_sort(arr)
   n = 0
+  l = arr.length - 2
   swapped = false
   sorted = false
 
@@ -13,22 +14,30 @@ def bubble_sort(arr)
     if arr[n] > arr[n+1]
       arr[n], arr[n+1] = arr[n+1], arr[n]
       swapped = true
-      if n == 4
+      if n == l
         n = 0
       else
         n+=1
       end
     else
-      if n == 4
+      if n == l
         n = 0
         swapped = false
       else
         n+=1
       end
     end
-    if n == 4 && swapped == false
+    if n == l && swapped == false
       sorted = true
     end
   end
   arr
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+      left.length - right.length
+    end
+
+def bubble_sort_by(arr)
+
 end
