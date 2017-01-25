d3f1bd6546c84697314c86f3519551b5a5c90497
Alan Cruse <alancruse86@gmail.com>
2017-01-25 22:44:33 +0100

    Completed all enumerable methods

--- project_two.rb
+++ project_two.rb
@@ -81,10 +81,36 @@ module Enumerable
       end
 
       def my_inject
-
+        sum = self[0]
+        self.my_each do |x|
+          unless x == self[0]
+          sum = yield(sum, x)
+          end
+        end
+        sum
       end
 
+      def my_map_2(proc)
+        new_arr = []
+        self.each do |x|
+            if block_given?
+              proc.call(x)
+              new_arr.push(proc.call(x))
+            else
+              yield(x)
+              new_arr.push(yield(x))
+            end
+          end
+        new_arr
+      end
     end
+
+    def multiply_els(arr)
+      return arr.my_inject { |sum, x| sum * x}
+    end
+
+    my_proc = Proc.new { |x| x.to_f }
+    
 
 arr = [1,2,3,4,5]
 puts arr.my_each { |x| puts x }
@@ -92,8 +118,21 @@ puts arr.my_each_with_index {|x, y| puts y }
 puts arr.my_select { |x| x.even? }
 puts arr.my_all? { |x| x.integer? }
 puts arr.my_all? { |x| x }
-puts arr.my_any? { |x| x == 6}
+puts arr.my_any? { |x| x == 6 }
 puts arr.my_none? { |x| x > 6 }
 puts arr.my_count { |x| x < 4 }
-puts arr.my_map { |x| x +=1 } 
+puts arr.my_map { |x| x +=1 }
+
+puts arr.my_inject { |sum, x| sum + x}
+puts multiply_els([2,4,5])
+#puts arr.my_inject_test #{ |sum, x| sum + x }
+
+
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+puts longest
+
+
+puts arr.my_map_2(my_proc) { |x| x += 1 }
 puts "Your program works!"
