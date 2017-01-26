8408b4244e17e11cb0165a0fd18b09ecc8bcd173
Alan Cruse <alancruse86@gmail.com>
2017-01-26 11:24:13 +0100

    More refactoring

--- project_two.rb
+++ project_two.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# The task of project two is to recreate the Ruby Enumerable methods
+# My recreated methods are below
+
 module Enumerable
   def my_each
     i = 0
@@ -61,6 +64,8 @@ module Enumerable
     counter
   end
 
+# For my version of the map method I was tasked to make sure that if given both a Proc and a block that only the Proc would run
+
   def my_map(proc = nil)
     new_arr = []
     self.my_each do |x|
@@ -82,6 +87,8 @@ module Enumerable
   end
 end
 
+# Below are the results of my methods compared to the original Enumerable methods
+
 def multiply_els(arr)
   arr.my_inject { |sum, x| sum * x }
 end
@@ -89,28 +96,34 @@ end
 my_proc = Proc.new { |x| x.to_f }
 
 arr = [1, 2, 3, 4, 5]
+puts "#each"
 puts arr.my_each { |x| puts x }
+puts arr.each { |x| puts x }
+puts "#each_with_index"
 puts arr.my_each_with_index { |x, y| puts "#{x} #{y}" }
+puts arr.each_with_index { |x, y| puts "#{x} #{y}" }
+puts "#select"
 puts arr.my_select { |x| x.even? }
-puts
+puts arr.select { |x| x.even? }
+puts "#all?"
 puts arr.my_all? { |x| x.integer? }
-puts
+puts arr.all? { |x| x.integer? }
+puts "#any?"
 puts arr.my_any? { |x| x == 6 }
-puts
+puts arr.any? { |x| x == 6 }
+puts "#none?"
 puts arr.my_none? { |x| x > 6 }
-puts
+puts arr.none? { |x| x > 6 }
+puts "#count"
 puts arr.my_count { |x| x < 4 }
-puts
+puts arr.count { |x| x < 4 }
+puts "#map"
 puts arr.my_map { |x| x += 1 }
+puts arr.map { |x| x += 1 }
 puts
 puts arr.my_map(my_proc) { |x| x += 1 }
-puts
+puts "#inject"
 puts arr.my_inject { |sum, x| sum + x }
+puts arr.inject { |sum, x| sum + x }
 puts
 puts multiply_els([2, 4, 5])
-longest = %w(cat sheep bear).my_inject do |memo, word|
-  memo.length > word.length ? memo : word
-end
-puts longest
-puts
-puts "Your program works!"
